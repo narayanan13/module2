9b776b7bc993faba867fb5e199b1facef7601272
narayanan13 <narayananofficial13@gmail.com>
2023-09-26 12:03:04 +0530

    productSchemainitial

---
+++ Schema/productSchema.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+
+const productSchema = new mongoose.Schema({
+    productName:{
+        type:String,
+        required:true,
+    },
+    categoryName:{
+        type:String,
+        required:true,
+    },
+    dateType:{
+        type:String,
+        required:true,
+    },
+    expiryDate:{
+        type:String,
+        required:true,
+    }
+})
+
+const product = mongoose.model('Product',productSchema);
+module.exports = product
